f85d0254e61732f7ad17e205abe681806a6b65d5
Alexandre do Vale <vale.apv@gmail.com>
2017-08-17 18:13:22 -0300

    Finalização da section principal
    Ajuste da "Dica de Leitura", botões para "Experiência" e "Blog".

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-//Menu
 $(document).ready(function() {
+    //Efeito menu
     $(window).scroll(function() {
         if($(document).scrollTop() > 60) {
             $('.navbar').addClass('alterarMenu');
@@ -11,4 +11,10 @@ $(document).ready(function() {
             $('.navbar-inverse .navbar-nav>li>a').css('color','#FF8420');
         }
     });
+    //Uma dica a cada load
+    var imagens = ['a-chegada.JPG',
+                   'alien-o-oitavo-passageiro.JPG',
+                   'scrum-a-arte.JPG'];
+    
+    $('<img class="dica-efeito img-responsive" src="img/livros/'+imagens[Math.floor(Math.random()*imagens.length)]+'">').appendTo('#dica-livro');
 });
