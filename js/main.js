1d5e70d2bf05b9e0be9b382c9c4f7209eb462d65
Alexandre do Vale <vale.apv@gmail.com>
2017-08-17 13:41:32 -0300

    Definição da Animação do menu e conteúdo do Jumbotron
    Estabeleci regras de CSS para os elementos e comecei a área de "Dica de
    Leitura".

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
+//Menu
 $(document).ready(function() {
     $(window).scroll(function() {
-        if($(document).scrollTop() > 10) {
+        if($(document).scrollTop() > 60) {
             $('.navbar').addClass('alterarMenu');
+            $('.navbar-inverse .navbar-nav>li>a').css('color','#211A9D');
+            
         }
         else {
             $('.navbar').removeClass('alterarMenu');
+            $('.navbar-inverse .navbar-nav>li>a').css('color','#FF8420');
         }
     });
 });
