6488a23be95c8219cb025d97ccba6eec0959fe42
Alexandre do Vale <vale.apv@gmail.com>
2017-08-17 21:45:28 -0300

    Adicção do post-it e a criação da section #conteudos
    15 frases randômicas carregando o post-it e movi a "Dica de Leitura"
    para a section de conteúdos.

--- js/main.js
+++ js/main.js
@@ -11,6 +11,28 @@ $(document).ready(function() {
             $('.navbar-inverse .navbar-nav>li>a').css('color','#FF8420');
         }
     });
+    //Um novo mindset a cada load
+    var mindset = ['Peça ajuda quando uma tarefa for difícil de mais pra iniciar ou terminar.',
+                   'Fique atendo, identifique seu "ladrão de foco" e remova distrações',
+                   'Utilize listas com sabedoria: mantenha a lista enxuta, não crie tarefas sem relevância, isso pode tirar seu foco e criar a sensação de que você esta trabalhando muito, quando na verdade esta desperdiçando energia.',
+                   'Utilize a técnica pomodoro para criar ciclos de produtividade',
+                   'Microlearning: forma de oferecer aprendizagem em doses menores, planejadas e estruturadas para serem de fácil e rápida assimilação.',
+                   'Um dos principais vilões no EAD é a distração causada por motivações que causam sensação de recompensa fácil, geralmente associada a procrastinação.',
+                   'Técnica Pomodoro (timer): 25 minutos de trabalho sem interrupção + 5 minutos de pausa',
+                   'No  design Thinking, a  Arte se junta à Ciência e à Tecnologia para encontrar novas soluções de negócio.',
+                   'Design Thinking = Arte + Ciência + Tecnologia',
+                   'Não "ache”, tenha certeza, pois maneiras subjetivas são barreiras para uma conversa construtivas.',
+                   '"Existem dias que mudam totalmente a sua vida." Entender e aceitar o tempo, quando digo sim para algo estou dizendo não para outras possibilidades.',
+                   'A essência precede a existência, isto é, um gato não tem opção de não ser um gato, ele já nasce com preceitos definidos. O ser humano é uma tela em branco, podemos ser primais, selvagens ou extremamente civilizados e evoluídos tecnologicamente e espiritualmente.',
+                   'Custa caro (tempo) treinar e educar uma equipe. Isso exige muita energia e o resultado será notado em longo prazo, não é um trabalho da noite para o dia.',
+                   'Liderança é um processo de criação em conjunto. Se você não se doar pela empresa sua equipe também não vai doar.',
+                   'Não se pode viver com medo das possibilidades.'];
+    
+    $('<p class="mindset">'+mindset[Math.floor(Math.random()*mindset.length)]+'</p>').appendTo('#post-it');
+    
+    var coresPostit = ['#5DFF9E', '#fefabc', '#FFB580', '#FFA3F0', '#A9BAFF']; 
+    var coresAleatorias = coresPostit[Math.floor(Math.random()*coresPostit.length)];
+    $('#post-it').css('background-color',coresAleatorias);
     //Uma dica a cada load
     var imagens = ['a-chegada.JPG',
                    'alien-o-oitavo-passageiro.JPG',
